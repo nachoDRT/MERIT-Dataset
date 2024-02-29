7ef291fbc6dc41124ddf544a331ab8270ae37ebf
nachoDRT <iderodrigo@comillas.edu>
2024-02-29 13:43:48 +0100

    Add Benchmark: LayoutLMv2 Addapted to LSD Dataset Samples
    Dockerized solution for RTX2080 Nvidia GPU

---
+++ benchmark/layoutlmv2/src/load_funsd_format.py
@@ -0,0 +1,231 @@
+# coding=utf-8
+import json
+import os
+
+import datasets
+from PIL import Image
+
+SUBJECTS_SEMANTIC_JSON = "/app/assets/subjects_english.json"
+ZIP_FILE_PATH = "/app/data/dataset.zip"
+
+FUNSD_LABELS = [
+    "O",
+    "B-HEADER",
+    "I-HEADER",
+    "B-QUESTION",
+    "I-QUESTION",
+    "B-ANSWER",
+    "I-ANSWER",
+]
+
+
+logger = datasets.logging.get_logger(__name__)
+
+
+_CITATION = """\
+@article{Jaume2019FUNSDAD,
+  title={FUNSD: A Dataset for Form Understanding in Noisy Scanned Documents},
+  author={Guillaume Jaume and H. K. Ekenel and J. Thiran},
+  journal={2019 International Conference on Document Analysis and Recognition Workshops (ICDARW)},
+  year={2019},
+  volume={2},
+  pages={1-6}
+}
+"""
+_DESCRIPTION = """\
+https://guillaumejaume.github.io/FUNSD/
+"""
+
+
+def load_image(image_path):
+    image = Image.open(image_path).convert("RGB")
+    w, h = image.size
+    return image, (w, h)
+
+
+def normalize_bbox(bbox, size):
+    return [
+        int(1000 * bbox[0] / size[0]),
+        int(1000 * bbox[1] / size[1]),
+        int(1000 * bbox[2] / size[0]),
+        int(1000 * bbox[3] / size[1]),
+    ]
+
+
+class FunsdConfig(datasets.BuilderConfig):
+    """BuilderConfig for FUNSD"""
+
+    def __init__(self, **kwargs):
+        """BuilderConfig for FUNSD.
+
+        Args:
+          **kwargs: keyword arguments forwarded to super.
+        """
+        super(FunsdConfig, self).__init__(**kwargs)
+
+
+class Funsd(datasets.GeneratorBasedBuilder):
+    """FUNSD dataset."""
+
+    BUILDER_CONFIGS = [
+        FunsdConfig(
+            name="LSD", version=datasets.Version("1.0.0"), description="FUNSD dataset"
+        ),
+    ]
+
+    def get_dataset_labels(self):
+
+        # Open '.json' file with info about subjects' synonyms
+        with open(SUBJECTS_SEMANTIC_JSON, "r") as subjects_json_file:
+            data = subjects_json_file.read()
+            subjects_semantic = json.loads(data)
+
+        subjects_json_file.close()
+
+        lsd_labels = FUNSD_LABELS
+        # Loop over the dictionary to extract its keys
+        for key in subjects_semantic["subjects"]:
+
+            # Construct labels strings and append
+            b_label = "".join(["B-", key.upper()])
+            lsd_labels.append(b_label)
+            i_label = "".join(["I-", key.upper()])
+            lsd_labels.append(i_label)
+            b_answer_label = "".join([b_label, "_ANSWER"])
+            lsd_labels.append(b_answer_label)
+            i_answer_label = "".join([i_label, "_ANSWER"])
+            lsd_labels.append(i_answer_label)
+
+        return lsd_labels
+
+    def _expand_labels_with_academic_years(
+        self, lsd_labels: list, academic_years: list, names_labels: list
+    ):
+
+        for academic_year in academic_years:
+
+            names_labels.append(academic_year[1:].upper())
+            lsd_labels.append("".join(["B-", academic_year[1:].upper()]))
+            lsd_labels.append("".join(["I-", academic_year[1:].upper()]))
+
+        return lsd_labels, names_labels
+
+    def get_dataset_labels_v2(self):
+
+        # Open '.json' file with info about subjects' synonyms
+        with open(SUBJECTS_SEMANTIC_JSON, "r") as subjects_json_file:
+            data = subjects_json_file.read()
+            subjects_semantic = json.loads(data)
+
+        subjects_json_file.close()
+
+        lsd_labels = FUNSD_LABELS
+        names_labels = []
+        # Loop over the dictionary to extract its keys
+        expanded_labels = []
+        expanded_tags = subjects_semantic["academic_years_tags"]
+        lsd_labels, names_labels = self._expand_labels_with_academic_years(
+            lsd_labels, expanded_tags, names_labels
+        )
+
+        for key in subjects_semantic["subjects"]:
+            for tag in expanded_tags:
+                expanded_labels.append("".join([key, tag]))
+
+        for key in expanded_labels:
+
+            # Construct labels strings and append
+            names_labels.append(key.upper())
+            names_labels.append("".join([key.upper(), "_ANSWER"]))
+            b_label = "".join(["B-", key.upper()])
+            lsd_labels.append(b_label)
+            i_label = "".join(["I-", key.upper()])
+            lsd_labels.append(i_label)
+            b_answer_label = "".join([b_label, "_ANSWER"])
+            lsd_labels.append(b_answer_label)
+            i_answer_label = "".join([i_label, "_ANSWER"])
+            lsd_labels.append(i_answer_label)
+
+        return lsd_labels, names_labels
+
+    def _info(self):
+
+        lsd_labels, _ = self.get_dataset_labels_v2()
+
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "id": datasets.Value("string"),
+                    "words": datasets.Sequence(datasets.Value("string")),
+                    "bboxes": datasets.Sequence(
+                        datasets.Sequence(datasets.Value("int64"))
+                    ),
+                    "ner_tags": datasets.Sequence(
+                        datasets.features.ClassLabel(names=lsd_labels)
+                    ),
+                    "image_path": datasets.Value("string"),
+                }
+            ),
+            supervised_keys=None,
+            homepage="https://guillaumejaume.github.io/FUNSD/",
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+
+        downloaded_file = dl_manager.extract(ZIP_FILE_PATH)
+
+        l = [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={"filepath": f"{downloaded_file}/dataset/training_data/"},
+            ),
+            datasets.SplitGenerator(
+                name=datasets.Split.TEST,
+                gen_kwargs={"filepath": f"{downloaded_file}/dataset/testing_data/"},
+            ),
+        ]
+
+        return l
+
+    def _generate_examples(self, filepath):
+        logger.info("⏳ Generating examples from = %s", filepath)
+        ann_dir = os.path.join(filepath, "annotations")
+        img_dir = os.path.join(filepath, "images")
+        for guid, file in enumerate(sorted(os.listdir(ann_dir))):
+            words = []
+            bboxes = []
+            ner_tags = []
+            file_path = os.path.join(ann_dir, file)
+            with open(file_path, "r", encoding="utf8") as f:
+                data = json.load(f)
+            image_path = os.path.join(img_dir, file)
+            image_path = image_path.replace("json", "png")
+            image, size = load_image(image_path)
+            for item in data["form"]:
+                words_example, label = item["words"], item["label"]
+                words_example = [w for w in words_example if w["text"].strip() != ""]
+                if len(words_example) == 0:
+                    continue
+                if label == "other":
+                    for w in words_example:
+                        words.append(w["text"])
+                        ner_tags.append("O")
+                        bboxes.append(normalize_bbox(w["box"], size))
+                else:
+                    words.append(words_example[0]["text"])
+                    ner_tags.append("B-" + label.upper())
+                    bboxes.append(normalize_bbox(words_example[0]["box"], size))
+                    for w in words_example[1:]:
+                        words.append(w["text"])
+                        ner_tags.append("I-" + label.upper())
+                        bboxes.append(normalize_bbox(w["box"], size))
+            yield guid, {
+                "id": str(guid),
+                "words": words,
+                "bboxes": bboxes,
+                "ner_tags": ner_tags,
+                "image_path": image_path,
+            }
---
+++ benchmark/layoutlmv2/src/train_layoutlmv2.py
@@ -0,0 +1,180 @@
+from datasets import load_dataset, load_metric
+from PIL import Image
+from transformers import (
+    LayoutLMv2Processor,
+    LayoutLMv2ForTokenClassification,
+    TrainingArguments,
+    Trainer,
+)
+from datasets import Features, Sequence, ClassLabel, Value, Array2D, Array3D
+import os
+from torch.utils.data import DataLoader
+import numpy as np
+import wandb
+import json
+
+LOAD_DATASET_FROM_PY = "/app/src/load_funsd_format.py"
+WANDB_LOGGING_PATH = "/app/config/wandb_logging.json"
+
+MAX_TRAIN_STEPS = 1000
+EVAL_FRECUENCY = 50
+
+
+class FunsdTrainer(Trainer):
+    def __init__(self, model, args, compute_metrics):
+        super(FunsdTrainer, self).__init__(
+            model=model,
+            args=args,
+            compute_metrics=compute_metrics,
+            eval_dataset=test_dataset,
+        )
+
+    def get_train_dataloader(self):
+        return train_dataloader
+
+    def get_test_dataloader(self, test_dataset):
+        return test_dataloader
+
+
+def preprocess_data(examples):
+    images = [Image.open(path).convert("RGB") for path in examples["image_path"]]
+    words = examples["words"]
+    boxes = examples["bboxes"]
+    word_labels = examples["ner_tags"]
+
+    encoded_inputs = processor(
+        images=images,
+        text=words,
+        boxes=boxes,
+        word_labels=word_labels,
+        padding="max_length",
+        truncation=True,
+    )
+    return encoded_inputs
+
+
+def compute_metrics(p):
+    predictions, labels = p
+    predictions = np.argmax(predictions, axis=2)
+
+    # Remove ignored index (special tokens)
+    true_predictions = [
+        [id2label[p] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+    true_labels = [
+        [id2label[l] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+
+    results = metric.compute(predictions=true_predictions, references=true_labels)
+    if return_entity_level_metrics:
+        # Unpack nested dictionaries
+        final_results = {}
+        for key, value in results.items():
+            if isinstance(value, dict):
+                for n, v in value.items():
+                    final_results[f"{key}_{n}"] = v
+            else:
+                final_results[key] = value
+        return final_results
+    else:
+        return {
+            "precision": results["overall_precision"],
+            "recall": results["overall_recall"],
+            "f1": results["overall_f1"],
+            "accuracy": results["overall_accuracy"],
+        }
+
+
+# Logging in wandb
+with open(WANDB_LOGGING_PATH) as f:
+    wandb_config = json.load(f)
+
+    wandb.login()
+    wandb.init(
+        project=wandb_config["project"],
+        entity=wandb_config["entity"],
+        name=wandb_config["name"],
+    )
+
+# Load dataset using a '.py' file
+datasets = load_dataset(LOAD_DATASET_FROM_PY, trust_remote_code=True)
+
+labels = datasets["train"].features["ner_tags"].feature.names
+
+id2label = {v: k for v, k in enumerate(labels)}
+label2id = {k: v for v, k in enumerate(labels)}
+
+# Load (pre) processor
+processor = LayoutLMv2Processor.from_pretrained(
+    "microsoft/layoutlmv2-base-uncased", revision="no_ocr"
+)
+
+features = Features(
+    {
+        "image": Array3D(dtype="int64", shape=(3, 224, 224)),
+        "input_ids": Sequence(feature=Value(dtype="int64")),
+        "attention_mask": Sequence(Value(dtype="int64")),
+        "token_type_ids": Sequence(Value(dtype="int64")),
+        "bbox": Array2D(dtype="int64", shape=(512, 4)),
+        "labels": Sequence(ClassLabel(names=labels)),
+    }
+)
+
+# Preprocess train and test datasets
+train_dataset = datasets["train"].map(
+    preprocess_data,
+    batched=True,
+    remove_columns=datasets["train"].column_names,
+    features=features,
+)
+train_dataset.set_format(type="torch", device="cuda")
+
+test_dataset = datasets["test"].map(
+    preprocess_data,
+    batched=True,
+    remove_columns=datasets["test"].column_names,
+    features=features,
+)
+test_dataset.set_format(type="torch", device="cuda")
+
+# Dataloaders
+train_dataloader = DataLoader(
+    train_dataset, batch_size=4, shuffle=True, pin_memory=False
+)
+test_dataloader = DataLoader(test_dataset, batch_size=2, pin_memory=False)
+
+batch = next(iter(train_dataloader))
+
+
+model = LayoutLMv2ForTokenClassification.from_pretrained(
+    "microsoft/layoutlmv2-base-uncased", num_labels=len(label2id)
+)
+
+# Set id2label and label2id
+model.config.id2label = id2label
+model.config.label2id = label2id
+
+# Metrics
+metric = load_metric("seqeval")
+return_entity_level_metrics = True
+
+args = TrainingArguments(
+    output_dir=wandb_config["project"],  # name of directory to store the checkpoints
+    max_steps=MAX_TRAIN_STEPS,  # we train for a maximum of 1,000 batches
+    warmup_ratio=0.1,  # we warmup a bit
+    fp16=True,  # we use mixed precision (less memory consumption)
+    push_to_hub=False,  # after training, we'd like to push our model to the hub
+    # push_to_hub_model_id=f"layoutlmv2-finetuned-funsd-test", # this is the name we'll use for our model on the hub
+    # evaluation_strategy="steps",
+    # eval_steps=EVAL_FRECUENCY,
+    report_to="wandb",
+)
+
+# Initialize our Trainer
+trainer = FunsdTrainer(model=model, args=args, compute_metrics=compute_metrics)
+
+# Train
+trainer.train()
+wandb.finish()
