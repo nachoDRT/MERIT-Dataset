c646fa0be7431d6ee75dc2cb43f673c1827aa0f4
nachoDRT <iderodrigo@comillas.edu>
2025-06-26 10:31:57 +0200

    Paligemma for MERIT

--- benchmark/paligemma/src/start.sh
+++ benchmark/paligemma/src/start.sh
@@ -2,9 +2,13 @@ chmod +x src/train.py
 chmod +x src/inference.py
 
 
+# python src/train.py \
+#     --dataset_name de-Rodrigo/merit \
+#     --dataset_subset en-digital-seq \
+
 python src/train.py \
-    --dataset_name de-Rodrigo/merit \
-    --dataset_subset es-digital-seq \
+    --dataset_name dvgodoy/rvl_cdip_mini \
+    --dataset_subset rvl_cdip_mini \
 
 
 # python src/inference.py \
